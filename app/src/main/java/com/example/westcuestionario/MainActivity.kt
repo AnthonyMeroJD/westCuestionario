91cf749414ea234b869ce80424d970dfb942ed7a
Anthony <AnthonyMeroJD@hotmail.com>
2020-04-16 15:19:32 -0500

    v 0.1 implementacion firebase

--- app/src/main/java/com/example/westcuestionario/MainActivity.kt
+++ app/src/main/java/com/example/westcuestionario/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.westcuestionario
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var database:DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database=FirebaseDatabase.getInstance().reference
+        val ref=database.child("hola").child("nueva").setValue("dsadas")
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 }
