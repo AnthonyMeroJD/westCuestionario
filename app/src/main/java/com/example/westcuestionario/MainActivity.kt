20351382ddaf9c99facd98ab273fc273b06aced7
liak17 <luis113818@hotmail.com>
2020-04-27 00:13:11 -0500

    version con cartas sin espaciado

--- app/src/main/java/com/example/westcuestionario/MainActivity.kt
+++ app/src/main/java/com/example/westcuestionario/MainActivity.kt
@@ -1,28 +1,9 @@
 package com.example.westcuestionario
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.PopupWindow
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.PopupMenu
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.pregunta.*
-import java.io.BufferedReader
-import java.io.FileReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.nio.file.Path
 
 
 class MainActivity : AppCompatActivity() {
